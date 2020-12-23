a92c4fc37c480104214d699bba39cc913137b27e
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2020-12-23 06:23:20 +0530

    Initial Commit

---
+++ sustain-market-vendor-iOSTests/sustain_market_vendor_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  sustain_market_vendor_iOSTests.swift
+//  sustain-market-vendor-iOSTests
+//
+//  Created by Abhiraj Chatterjee on 12/23/20.
+//
+
+import XCTest
+@testable import sustain_market_vendor_iOS
+
+class sustain_market_vendor_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
